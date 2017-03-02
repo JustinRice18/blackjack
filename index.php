3aa072752e100bf070207829e3a3ee9ea84981ec
JustinRice18 <jrice18@amdg.brebeuf.org>
2017-03-02 08:48:32 -0500

    almost

--- index.php
+++ index.php
@@ -1,110 +1,164 @@
-
+<?php
+//requrie_once('index.php');
+session_start();
+?>
 <form action="index.php" method="get">
-    <input type="hidden" name="New Game" value="run">
-    <input type="submit" value="New Game">
-    <input type="hidden" name="Hit" value="run">
-    <input type="submit" value=" Hit">
+    <input type="submit" value="hit" name="action">
+    <input type="submit" value="pass" name="action">
+    <input type="submit" value="newgame" name="action">
 
 </form>
 <?php
 //requrie_once('index.php');
-session_start();
 
-if (isset($_SESSION)) {
-    if (isset($_GET['new']) && $_GET['new'] == "New") {
-        session_unset();
-    }
-}
-if(!isset($_SESSION["deck"])) $_SESSION["deck"] = array(
-        "Ace of Diamonds" => 1,
-        "Ace of Hearts" => 1,
-        "Ace of Clubs" => 1,
-        "Ace of Spades" => 1,
-        "Two of Diamonds" => 2,
-        "Two of Hearts" => 2,
-        "Two of Clubs" => 2,
-        "Two of Spades" => 2,
-        "Three of Diamonds" => 3,
-        "Three of Hearts" => 3,
-        "Three of Clubs" => 3,
-        "Three of Spades" => 3,
-        "Four of Diamonds" => 4,
-        "Four of Hearts" => 4,
-        "Four of Clubs" => 4,
-        "Four of Spades" => 4,
-        "Five of Diamonds" => 5,
-        "Five of Hearts" => 5,
-        "Five of Clubs" => 5,
-        "Five of Spades" => 5,
-        "Six of Diamonds" => 6,
-        "Six of Hearts" => 6,
-        "Six of Clubs" => 6,
-        "Six of Spades" => 6,
-        "Seven of Diamonds" => 7,
-        "Seven of Hearts" => 7,
-        "Seven of Clubs" => 7,
-        "Seven of Spades" => 7,
-        "Eight of Diamonds" => 8,
-        "Eight of Hearts" => 8,
-        "Eight of Clubs" => 8,
-        "Eight of Spades" => 8,
-        "Nine of Diamonds" => 9,
-        "Nine of Hearts" => 9,
-        "Nine of Clubs" => 9,
-        "Nine of Spades" => 9,
-        "Ten of Diamonds" => 10,
-        "Ten of Hearts" => 10,
-        "Ten of Clubs" => 10,
-        "Ten of Spades" => 10,
-        "Jack of Spades" => 10,
-        "Jack of Hearts" => 10,
-        "Jack of Diamonds" => 10,
-        "Jack of Clubs" => 10,
-        "Queen of Clubs" => 10,
-        "Queen of Spades" => 10,
-        "Queen of Hearts" => 10,
-        "Queen of Diamonds" => 10,
-        "King of Clubs" => 10,
-        "King of Diamonds" => 10,
-        "King of Hearts" => 10,
-        "King of Spades" => 10,);
+
+  if(isset($_GET['action'])) {
+$_SESSION['player']= 0;
+      switch ($_GET['action']) {
+          case "NewGame":
+              session_unset();
+              break;
+          case 'hit':
+               dealplayer();
+
+              break;
+          case 'pass';
+              dealcomputer();
+              break;
+          default;
+              break;
+      }
+
+
+
+      if (!isset($_SESSION["deck"])) {
+          $_SESSION["deck"] = array(
+              "Ace of Diamonds" => 1,
+              "Ace of Hearts" => 1,
+              "Ace of Clubs" => 1,
+              "Ace of Spades" => 1,
+              "Two of Diamonds" => 2,
+              "Two of Hearts" => 2,
+              "Two of Clubs" => 2,
+              "Two of Spades" => 2,
+              "Three of Diamonds" => 3,
+              "Three of Hearts" => 3,
+              "Three of Clubs" => 3,
+              "Three of Spades" => 3,
+              "Four of Diamonds" => 4,
+              "Four of Hearts" => 4,
+              "Four of Clubs" => 4,
+              "Four of Spades" => 4,
+              "Five of Diamonds" => 5,
+              "Five of Hearts" => 5,
+              "Five of Clubs" => 5,
+              "Five of Spades" => 5,
+              "Six of Diamonds" => 6,
+              "Six of Hearts" => 6,
+              "Six of Clubs" => 6,
+              "Six of Spades" => 6,
+              "Seven of Diamonds" => 7,
+              "Seven of Hearts" => 7,
+              "Seven of Clubs" => 7,
+              "Seven of Spades" => 7,
+              "Eight of Diamonds" => 8,
+              "Eight of Hearts" => 8,
+              "Eight of Clubs" => 8,
+              "Eight of Spades" => 8,
+              "Nine of Diamonds" => 9,
+              "Nine of Hearts" => 9,
+              "Nine of Clubs" => 9,
+              "Nine of Spades" => 9,
+              "Ten of Diamonds" => 10,
+              "Ten of Hearts" => 10,
+              "Ten of Clubs" => 10,
+              "Ten of Spades" => 10,
+              "Jack of Spades" => 10,
+              "Jack of Hearts" => 10,
+              "Jack of Diamonds" => 10,
+              "Jack of Clubs" => 10,
+              "Queen of Clubs" => 10,
+              "Queen of Spades" => 10,
+              "Queen of Hearts" => 10,
+              "Queen of Diamonds" => 10,
+              "King of Clubs" => 10,
+              "King of Diamonds" => 10,
+              "King of Hearts" => 10,
+              "King of Spades" => 10,);
+      }
+
+
+  }
+  function rules(){
+
+
+
+  }
+
+
+
+
+
+function dealplayer()
 {
-    $_SESSION['house'] = 0;
-    $_SESSION['player'] = 0;
-    $_SESSION['Used'] =[];
+    $pick2 = rand(2, 2);
 
-    if (!empty($_GET['Hit'])) {
-        hit();
+    $randomkey = array_rand($_SESSION["deck"], $pick2 );
+    if($_SESSION['player']=== 0)
+    foreach ($randomkey as $key) {
+        $end =implode($randomkey, " and ");
+
+
+        echo "you received " .$end . " bringing your total to " . $_SESSION['deck'][$key], '<br />';
+        ($_SESSION['player'] += $_SESSION['deck'][$key]);
     }
-    if (!empty($_GET['New Game'])) {
-        newGame();
-if(    $_SESSION['player'] == 21 and $_SESSION['house'] > 21 ){
-    echo "You Win";
+    elseif($_SESSION['player']>0 && $_SESSION['player'] < 21){
+        $pick1 = rand(1,1);
+        $randomkeys = array_rand($_SESSION["deck"], $pick1 );
+        foreach ($randomkeys as $keys) {
+            echo "you received " .$randomkeys. " bringing your total to " .( $_SESSION['player']+$keys); '<br />';
+            ($_SESSION['player'] += $_SESSION['deck'][$keys]);
 
-}elseif($_SESSION['house'] == 21 and $_SESSION['player'] > 21 ){
-    echo "You Lose. Play Again?";
+        }
 
-
-
-
-    }
+     }
 
 }
+/*
+if($_SESSION['player']= 0); {
+    foreach ($play as $x => $x_value) {
+        echo "you pulled" . $x . ", bringing your total to" . $x_value;
+        echo "<br>";
+    }
+*/
 
 
 
 
 Function hit(){
-    echo array_rand($_SESSION["deck"]);
+    foreach ($_SESSION['deck'] as $value => $key);
+
+    $play = array_rand($_SESSION["deck"]);
+    echo $play;
+    echo $_SESSION['player'] =+$play;
 }
 
 Function newGame(){
-    $play = array_rand($_SESSION["deck"],2);
-  echo $play;
-}
-
+    $play = array_rand($_SESSION["deck"]);
+    echo $play;
 
 }
+function drawTwo(){
+    shuffle($_SESSION['deck']);
+}
+
+// echo array_values($play);
+   // unset($_SESSION["deck"][$play]);
+
+
+
+
+
 /*if(isset($_GET['action'])){
     switch($_GET('action')){
         case'shuffle';
